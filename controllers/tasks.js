bf12831fbec01d97ec409f5092a4e5237e8303ba
trey_weathers <treychelle.weathers2017@gmail.com>
2023-04-01 06:18:13 -0400

    minor edits to controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -15,13 +15,13 @@ res.json(task)
 })
 },
 edit: (req, res) => {
-Task.findOneAndUpdate({name: req.params.name}, req.body)
+Task.findOneAndUpdate({name: req.params.title}, req.body)
 .then(task => {
 res.json(task)
 })
 },
 delete: (req, res) => {
-Task.findOneAndDelete({name: req.params.name})
+Task.findOneAndDelete({name: req.params.title})
 .then(task => {
 res.json(task)
 })
